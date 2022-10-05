ca31c33a46f539b558d7cf2b4f2e07929d7ce023
Andres Rodriguez <rod16015@byui.edu>
2022-10-05 13:20:35 -0300

    new changes added

--- lib/screens/alertscreens.dart
+++ lib/screens/alertscreens.dart
@@ -17,7 +17,7 @@ class AlertScreen extends StatelessWidget {
            ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.lightBlue
+        backgroundColor: Colors.lightBlue,
         child: const Icon(Icons.close),
         onPressed: () {
          Navigator.pop(context);
